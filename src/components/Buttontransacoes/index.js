ab59712370588c5833940f42437e7df9f434a91e
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-04-02 20:19:09 -0300

    header componentizado funcionando

--- src/components/Buttontransacoes/index.js
+++ src/components/Buttontransacoes/index.js
@@ -1,7 +1,25 @@
 import './style.css';
+import { useState } from 'react';
 
 function ButtonTransacoes(){
+  const [openModal, setOpenModal] = useState(false);
+  const [editar, setEditar] = useState(false);
+  const [transacaoEditada, setTransacaoEditada] = useState(false);
 
+  const handleModal = () => {
+    setEditar(false);
+    setTransacaoEditada(false);
+    openModal ? setOpenModal(false) : setOpenModal(true);
+  }
+  
+  return(
+    <button
+      className="btn-add"
+      onClick={handleModal}
+    >
+      Adicionar Registro
+    </button>
+  )
 }
 
 export default ButtonTransacoes;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,12 @@ import './style.css';
 import logoDindin from '../../assets/logoDindin.svg';
 
 function Header(){
+  return(
     <header className='container-header'>
       <img src={logoDindin} alt="Logo Dindin" className='logo' />
       <h1 className='titulo'>Dindin</h1>
     </header>
+  )
 }
 
 export default Header;
