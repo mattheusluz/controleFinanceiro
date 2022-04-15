6d69ee700d59677a366e4153bf8e7c36e311faa6
mattheusluz <80133489+mattheusluz@users.noreply.github.com>
2022-04-15 17:34:35 -0300

    Implementada conexão com API no cadastro de usuarios

--- src/Pages/Cadastro/index.js
+++ src/Pages/Cadastro/index.js
@@ -1,41 +1,163 @@
+import { useState } from 'react';
 import './style.css';
 import '../../globalStyles/styles.css';
 import HeaderLogin from '../../components/HeaderLogin';
-import {NavLink} from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 
-function Cadastro(){
-  return(
+function Cadastro() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [repetirSenha, setRepetirSenha] = useState('');
+  const [erroCadastro, setErroCadastro] = useState(null);
+
+  const history = useHistory();
+
+  const cadastroUsuario = async (e) => {
+    e.preventDefault();
+    setErroCadastro(null);
+
+    try {
+      const dados = {
+        nome,
+        email,
+        senha
+      }
+
+      if (senha.length === 0) {
+        setErroCadastro('senha é um campo obrigatório');
+        return;
+      }
+
+      if (senha !== repetirSenha || repetirSenha.length === 0) {
+        setErroCadastro('As senhas não conferem');
+        return;
+      }
+
+      const resposta = await fetch('https://sistemacontrolefinanceiro.herokuapp.com/usuarios', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dados),
+      });
+
+      const data = await resposta.json();
+
+      if (data.erro) {
+        setErroCadastro(data.erro);
+        return
+      }
+
+      history.push('/cadastroConcluido');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
     <>
-    <HeaderLogin className='header'/>
-    <div className='page'>
-      <div className="container">
-        <form type='submit' className='form'>
-          <h1 className='title'>Cadastrar usuário</h1>
-          <label htmlFor="nome" className='label'>
-            Nome Completo:
-          </label>
-          <input id='nome' type="text" className='input' placeHolder='Digite o nome completo' />
-          <label htmlFor="emailCadastro" className='label'>
-            E-mail:
-          </label>
-          <input id='emailCadastro' type="text" className='input' placeHolder='Digite o e-mail' />
-          <div className="senhas">
-            <label htmlFor="senha1" className='labelSenhas'>
-              Senha:
+      <HeaderLogin className='header' />
+      <div className='page'>
+        <div className="container">
+          <form onSubmit={cadastroUsuario} className='form'>
+            <h1 className='title'>Cadastrar usuário</h1>
+            <label htmlFor="nome" className='label'>
+              Nome Completo:
             </label>
-            <input id='senha1'type="password" className='inputSenha' placeHolder='Digite uma senha' />
-            <label htmlFor="senha2" className='labelSenhas'>
-              Confirmar Senha:
+            <input
+              id='nome'
+              type="text"
+              className={
+                erroCadastro
+                  && erroCadastro.includes('nome')
+                  ? 'erroInput input'
+                  : 'input'
+              }
+              placeholder='Digite o nome completo'
+              onChange={(e) => setNome(e.target.value)}
+              value={nome}
+            />
+            {
+              erroCadastro
+              && erroCadastro.includes('nome')
+              && < span className='spanErro' > {erroCadastro}</span>
+            }
+            <label htmlFor="emailCadastro" className='label'>
+              E-mail:
             </label>
-            <input id='senha2' type="password" className='inputSenha' placeHolder='Digite novamente' />
-          </div>
-        </form>
-          <div className="buttons">
-            <NavLink to={'/'} className='buttonVoltar'>Voltar</NavLink>
-            <NavLink to={'/cadastroConcluido'} className='buttonCadastrar'>Cadastrar</NavLink>
-          </div>
+            <input
+              id='emailCadastro'
+              type="text"
+              className={
+                erroCadastro
+                  && erroCadastro.includes('email')
+                  ? 'erroInput input'
+                  : 'input'
+              }
+              placeholder='Digite o e-mail'
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            {
+              erroCadastro
+              && erroCadastro.includes('email')
+              && < span className='spanErro' > {erroCadastro}</span>
+            }
+            <div className="senhas">
+              <label htmlFor="senha1" className='labelSenhas'>
+                Senha:
+              </label>
+              <input
+                id='senha1'
+                type="password"
+                className={
+                  erroCadastro
+                    && erroCadastro.includes('senha')
+                    ? 'erroInput inputSenha'
+                    : 'input'
+                }
+                placeholder='Digite uma senha'
+                onChange={(e) => setSenha(e.target.value)}
+                value={senha}
+              />
+              {
+                erroCadastro
+                && erroCadastro.includes('senha')
+                && < span className='spanErro' > {erroCadastro}</span>
+              }
+              <label
+                htmlFor="senha2"
+                className='labelSenhas'
+              >
+                Confirmar Senha:
+              </label>
+              <input
+                id='senha2'
+                type="password"
+                className={
+                  erroCadastro
+                    && erroCadastro.includes('senha')
+                    ? 'erroInput inputSenha'
+                    : 'input'
+                }
+                placeholder='Digite novamente'
+                onChange={(e) => setRepetirSenha(e.target.value)}
+                value={repetirSenha}
+              />
+              {
+                erroCadastro
+                && erroCadastro.includes('senha')
+                && < span className='spanErro' > {erroCadastro}</span>
+              }
+            </div>
+            <div className="buttons">
+              <NavLink to={'/'} className='buttonVoltar'>Voltar</NavLink>
+              <button type='submit' className='buttonCadastrar'>Cadastrar</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   )
 }
